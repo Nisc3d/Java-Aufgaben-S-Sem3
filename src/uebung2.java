a4fc83c1db030b165e753e2e1c14771d94f39457
Nisc3d <nisc3c@protonmail.com>
2019-10-21 12:00:07 +0200

    test

--- src/uebung2.java
+++ src/uebung2.java
@@ -6,23 +6,36 @@ public class uebung2 {
         try {
 
             double durchschnittsnote = 0;
+            int[] noten = new int[6];
             int lauf = 0;
             Connection Verbindung;
             Statement Abfrage;
             String db = "jdbc:mysql://localhost:3306/notenverwaltung";
             String user = "root";
             String pw = "";
-            String SQLString = "select note from klausurstudent;";
+            String SQLString = "select note from klausurstudent where " ;
             Verbindung = DriverManager.getConnection(db, user, pw);
             Abfrage = Verbindung.createStatement();
             ResultSet rs = Abfrage.executeQuery(SQLString);
-//            System.out.println(rs);
-            while (rs.next()) {
-                durchschnittsnote = rs.getDouble(1) + durchschnittsnote;
-                lauf += 1;
-            }
-            durchschnittsnote = durchschnittsnote / lauf;
-            System.out.println("Durschschnittsnote: " + durchschnittsnote);
+//            while (rs.next()) {
+//                durchschnittsnote = rs.getDouble(3) + durchschnittsnote;
+//                lauf += 1;
+//            }
+//            durchschnittsnote = durchschnittsnote / lauf;
+
+//            while (rs.next()) {
+//                for (int i = 1; i <= 10; i++) {
+//                    if (rs.getInt(3) == i) {
+//                        noten[i] += 1;
+//                    }
+//                }
+//            }
+
+//            System.out.println("Durschschnittsnote: " + durchschnittsnote);
+
+//            for (int i = 0; i < noten.length; i++) {
+//                System.out.println("Häufigkeit Note " + i + ": " + noten[i]);
+//            }
 
 
         } catch (Exception Fehler) {
