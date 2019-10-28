491a18a522b5c3c5310c12d94f3fd8cfbeb794c4
Nisc3d <nisc3c@protonmail.com>
2019-10-28 12:59:52 +0100

    test

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    public int martikelnr;
+    public double note;
+    public double durchschnittsnote;
+
+    Student(){
+        durchschnittsnote = 0;
+    }
+
+    public int getMartikelnr() {
+        return martikelnr;
+    }
+
+    public double getNote() {
+        return note;
+    }
+
+    public double getDurchschnittsnote() {
+        return durchschnittsnote;
+    }
+
+}
--- src/uebung2.java
+++ src/uebung2.java
@@ -8,15 +8,20 @@ public class uebung2 {
             double durchschnittsnote = 0;
             int[] noten = new int[6];
             int lauf = 0;
+            Student[] studenten = new Student[100];
             Connection Verbindung;
             Statement Abfrage;
             String db = "jdbc:mysql://localhost:3306/notenverwaltung";
             String user = "root";
             String pw = "";
-            String SQLString = "select note from klausurstudent where " ;
+//            String SQLString = "select * from klausurstudent";
+
             Verbindung = DriverManager.getConnection(db, user, pw);
-            Abfrage = Verbindung.createStatement();
-            ResultSet rs = Abfrage.executeQuery(SQLString);
+            PreparedStatement statement = Verbindung.prepareStatement("SELECT note from klausurstudent where martikelnr = ?;");
+            statement.setString(1, "12345");
+            ResultSet rs = statement.executeQuery();
+//            Abfrage = Verbindung.createStatement();
+//            ResultSet rs = Abfrage.executeQuery(SQLString);
 //            while (rs.next()) {
 //                durchschnittsnote = rs.getDouble(3) + durchschnittsnote;
 //                lauf += 1;
@@ -37,10 +42,42 @@ public class uebung2 {
 //                System.out.println("Häufigkeit Note " + i + ": " + noten[i]);
 //            }
 
+//            while (rs.next()) {
+//                if (lauf == 0) {
+//                    studenten[lauf] = new Student();
+//                    studenten[lauf].martikelnr = (int) rs.getDouble(1);
+//                    studenten[lauf] = new Student();
+//                    studenten[lauf].durchschnittsnote = rs.getDouble(3) + studenten[lauf].durchschnittsnote;
+//                    lauf += 1;
+//                }
+//                else {
+//                    if (studenten[lauf - 1].martikelnr != rs.getDouble(1)) {
+//                        studenten[lauf] = new Student();
+//                        studenten[lauf].martikelnr = (int) rs.getDouble(1);
+//                        studenten[lauf].durchschnittsnote = rs.getDouble(3) + studenten[lauf].durchschnittsnote;
+//                        lauf += 1;
+//                    }
+//                    else{
+//                        studenten[lauf-1].durchschnittsnote = rs.getDouble(3) + studenten[lauf].durchschnittsnote;
+//                        lauf += 1;
+//                    }
+//                }
+//            }
+//            int alterlauf = lauf;
+//            lauf = 0;
+//            while (lauf <= studenten.length) {
+//                studenten[lauf].durchschnittsnote = studenten[lauf].durchschnittsnote / alterlauf;
+//                lauf += 1;
+//            }
+            while (rs.next()){
+                durchschnittsnote = rs.getDouble(1) + durchschnittsnote;
+                lauf += 1;
+            }
+            durchschnittsnote = durchschnittsnote / lauf;
+            System.out.println("Durschschnittsnote: " + durchschnittsnote);
 
         } catch (Exception Fehler) {
-            System.out.println("Fehler im Programm: ");
-            System.out.println(Fehler.toString());
+            Fehler.printStackTrace();
             System.exit(0);
         }
     }
